6bb4c69f2c5f5b7d02f9ea3fa918fe70d989449e
paragkulkarni <paragkk80@gmail.com>
2019-06-24 11:02:59 +0530

    add project to git

---
+++ server/models/ServerPort.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ServerPort = new Schema(
+    {
+        name:{
+            type:String
+        },
+        port:{
+            type:Number
+        }
+    },{
+        collection: 'servers'
+    }
+);
+
+module.exports = mongoose.model('ServerPort', ServerPort);
